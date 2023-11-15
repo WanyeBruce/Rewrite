1792368f6a33696c3b7f217e998e9b97d217f7d4
WanyeBruce <128329447+WanyeBruce@users.noreply.github.com>
2023-11-15 14:45:02 +0800

    Duet.js

---
+++ Duet.js
@@ -0,0 +1,42 @@
+/******************************
+
+脚本名称: Duet
+下载地址：商店
+脚本作者：ios151
+更新时间：2023年11月15日 04:20
+问题反馈：https://t.me/ios151
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/WanyeBruce/Rewrite/main/Duet.js
+
+[mitm] 
+
+hostname = buy.itunes.apple.com
+
+*******************************/
+
+var objc = JSON.parse($response.body);
+
+    objc = {
+  "success": true,
+  "products": [
+    {
+      "vendor": "apple",
+      "product": "DuetAirAnnual",
+      "subscriptionId": 391969,
+      "purchaseDate": "2023-11-14T20:08:02Z",
+      "cancelled": false,
+      "expiresDate": "2099-11-21T20:07:56Z",
+      "inTrial": true
+    }
+  ],
+  "hasStripeAccount": false
+}
+
+$done({body : JSON.stringify(objc)});
+
