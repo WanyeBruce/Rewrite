0c726a309f0ede9ea3a5c02aadc6e282fcff7309
WanyeBruce <128329447+WanyeBruce@users.noreply.github.com>
2023-11-15 14:32:27 +0800

    Update and rename Scanner Pro to ScannerPro.js

--- ScannerPro.js
+++ ScannerPro.js
@@ -12,7 +12,7 @@
 [rewrite_local]
 
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body 
 
 [mitm] 
 
